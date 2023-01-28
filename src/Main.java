0bddd62c18404c70d1af1c4501ec4198f01d7831
MagaAVo <magammedavo@gmail.com>
2023-01-28 20:36:48 +0300

    newtik

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2500; // Цена билета
+        int rub = 20; // стоимость одного бонуса
+        int mile = ticket / rub;
+
+        System.out.println("Колличество начисленных миль" + mile);
+    }
+}
